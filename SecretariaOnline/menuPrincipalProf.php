f9268f7b53c015d8290caf2dc27d9aec73f1b43e
Gabriel <Gabriel>
2017-07-01 13:46:33 -0300

    Alteração do Menu Principal Professor!

--- SecretariaOnline/menuPrincipalProf.php
+++ SecretariaOnline/menuPrincipalProf.php
@@ -5,7 +5,8 @@
         <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
         <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css">
-        <link href="front/estiloProf.css" rel="stylesheet">
+         <link href="front/styleProf.css" rel="stylesheet">
+        
     </head><body>
         <div class="navbar navbar-default navbar-inverse navbar-static-top">
             <div class="container">
@@ -33,8 +34,12 @@
                     <div class="col-md-6">
                         <blockquote>
                             <font color="white">
-                                </font><p><font color="white">Olá,
-                                    
+                                </font>
+                                <p><font color="white">Olá,
+                                <br><h2>Professor<br></h2>
+                                </font>
+
+                                <font color="black">
                                     <?php 
 
                                      include "classeConexao.php";
@@ -43,52 +48,67 @@
                                      $resultado = mysql_fetch_assoc($query); 
                                      $nome = $resultado[ 'nome']; 
 
-                                     echo "<h2>Professor $nome</h2>";
-
+                                     echo "<h2><b>$nome</b></h2>";
+                                    
+                                    
+                                    
                                     ?>
-                                        <br>
-                                        <br>
+                                    
                             </font>
                     </p></blockquote></div>
 
                     <div class="col-md-6">
-                        <a href = "verServicos.php" class = "btn btn-block btn-warning"><font size="3" color="White">Ver Serviços Cadastrados</font></a>
+                        <a href = "" class = "btn btn-block btn-warning"><font size="3" color="White">Ver Serviços Pendentes</font></a>
                     </div>
 
                     <br>
 
                     <div class="col-md-6">
-                        <a href = "solicitarServ.php" class="btn btn-block btn-danger btn-sm"><font size="3" color="White">Solicitar Serviço</font></a>
+                       <a href = "" class="btn btn-block btn-danger btn-sm">
+                       <font size="3" color="White">Ativar Serviço</font></a>
                     </div>
 
                     <h3 class="text-center">
-                        <font color="white">Serviços Disponíveis:</font>
+                        <font color="white">Serviços Gerenciados:</font>
                     </h3>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <ul class="list-group">
-                                    <li class="list-group-item list-group-item-info">
-                                        <font color="#0f7be7">
-                                            <h4>Relatórios e Documentos de Estágio</h4>
-                                        </font>
-                                    </li>
-                                    <li class="list-group-item list-group-item-info">
-                                        <font color="#0f7be7">
-                                            <h4>Provas Substitutivas</h4>
-                                        </font>
-                                    </li>
-                                    <li class="list-group-item list-group-item-info">
-                                        <font color="#0f7be7">
-                                            <h4>Material Online e Dúvidas</h4>
-                                        </font>
-                                    </li>
-                                    <li class="list-group-item list-group-item-info">
-                                        <font color="#0f7be7">
-                                            <h4>Solicitar Reunião com Coordenador</h4>
-                                        </font>
-                                    </li>
-                                </ul>
-    
+                    <br> 
 
-</div></div></div></div></div></div></body></html>
+                    <table border = "0" align = "center">
+                        <tr>
+                                <td width="150px"> 
+                                    <h4 class="text-center" align = "right">
+                                    <font color="white">Relatórios de Estagio</font>
+                                    </h4>
+                                </td>
+
+                                <td width="5px">    
+                                    <h4 class="text-center">
+                                    <font color="white">Provas Substitutivas</font>
+                                    </h4>
+                                </td>
+                        </tr>
+
+                           <tr>
+                                <td width="150px"> 
+                                     <h4 class="text-center">
+                                     <font color="white">Material Online e Dúvidas</font>
+                                     </h4>
+                                </td>
+
+                                <td>    
+                                    <h4 align = "center">
+                                    <font color="white">Reunião com Coordenador</font>
+                                    </h4>
+                                </td>
+                        </tr>
+                    
+                   
+                   
+                    
+
+                  
+                 </div>
+            </div>
+        </div>
+    </body>
+</html>
